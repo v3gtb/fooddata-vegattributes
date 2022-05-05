d91a0db5e446b018fd1e89a20ec1a9f240e0d9db
v3gtb <v3gtb@inbox.lv>
2022-05-05 20:41:15 +0200

    Use TypedDict instead of NewType for FoodDataDict

--- fooddata_vegattributes/fooddata.py
+++ fooddata_vegattributes/fooddata.py
@@ -1,7 +1,17 @@
 import json
-from typing import cast, Dict, List, NewType
+from typing import cast, List, TypedDict
 
-FoodDataDict = NewType("FoodDataDict", Dict)
+# NOTE that while the TypedDict PEP (PEP-589) and docs both suggest that extra
+# keys are not allowed, this is actually contentious because it contradicts
+# structural subtyping - refer to the discussion about transitivity in
+# https://mail.python.org/archives/list/typing-sig@python.org/thread/66RITIHDQHVTUMJHH2ORSNWZ6DOPM367
+# In any event, we want to use this class to refer to dicts that MUST contain
+# the specified keys and MAY contain any other extra keys. But to comply with
+# the current (inconsistent) definition of TypedDict, we'll just typing.cast()
+# back and forth to regular dict where required.
+class FoodDataDict(TypedDict):
+    fdcId: int
+    description: str
 
 def load_survey_fooddata_dicts() -> List[FoodDataDict]:
   with open("FoodData_Central_survey_food_json_2021-10-28.json") as f:
