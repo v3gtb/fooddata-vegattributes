23e4db0f44b595fe12e4cf9fa6e0402b0c11ec04
v3gtb <v3gtb@inbox.lv>
2022-05-24 00:12:25 +0200

    Allow padding in select_n_random

--- fooddata_vegattributes/utils/random.py
+++ fooddata_vegattributes/utils/random.py
@@ -1,16 +1,28 @@
 import random
+from typing import Callable, List, Optional, Sequence, TypeVar
 
-def select_n_random(items, n, criterion=None):
+
+T = TypeVar("T")
+
+def select_n_random(
+  items: Sequence[T],
+  n: int,
+  criterion: Optional[Callable[[T], bool]]=None,
+  pad: Optional[Callable[[], T]]=None,
+) -> List[T]:
   indices_todo = list(range(len(items)))
-  selected = []
+  selected: List[T] = []
   while len(selected) < n:
     if not indices_todo:
-      if criterion is not None:
+      if pad:
+        selected.extend(pad() for _ in range(n-len(selected)))
+        break
+      else:
         error_msg = (
           "not enough items fulfilling criterion in given sequence"
           if criterion is not None else "not enough items in given sequence"
         )
-      raise ValueError(error_msg)
+        raise ValueError(error_msg)
     i = random.choice(indices_todo)
     indices_todo.remove(i)
     item = items[i]
