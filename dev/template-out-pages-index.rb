90c9315b545449c2b4da8ce21126c8ac5315fb0e
v3gtb <v3gtb@inbox.lv>
2022-07-27 01:21:12 +0200

    Clean up script to template out index.md a bit

--- dev/template-out-pages-index.rb
+++ dev/template-out-pages-index.rb
@@ -1,25 +1,12 @@
 #!/usr/bin/env ruby
 require 'jekyll'
 
-#s = Jekyll::Site.new({
-  #"source" => Dir.pwd,
-  #"destination" => "/tmp",
-  #"permalink" => "http://example.net",
-  #"liquid" => {
-    #"error_mode" => "warn"
-  #},
-  #"limit_posts" => 0,
-  #"plugins" => [],
-  #"collections_dir" => "/tmp",
-#})
 s = Jekyll::Site.new(Jekyll.configuration({ "safe": true }))
 s.read()
 c = Jekyll::Collection.new(s, "some-label")
 d = Jekyll::Document.new("index.md", { :site => s, :collection => c })
 d.read()
 r = Jekyll::Renderer.new(s, d)
-r.send :assign_pages!
-r.send :assign_current_document!
 info = {
   :registers        => { :site => s, :page => r.payload["page"] },
   :strict_filters   => true,
